9a57a0e4d0153ae08abb58907041c3c72dcb456c
Nitin Vishvakarma <113825754+nitinvishwakarma1@users.noreply.github.com>
2023-12-15 00:40:59 +0530

    Delete Desktop/projects/VCLT_Project4 directory
    i had deleted vclt_project4

--- Desktop/projects/VCLT_Project4/backend/model/connection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb://127.0.0.1:27017/vclt", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('connection sucessful'))
-    .catch((err) => console.log(err));
-
-export default mongoose;
